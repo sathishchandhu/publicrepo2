4719e33c114e7de1fd8b325bf08aada94e577686
sathish chandhu <javabigdata9@gmail.com>
2015-01-26 22:46:06 -0500

    *.java

--- NumberGuessingGameV7.java
+++ NumberGuessingGameV7.java
@@ -1,6 +1,8 @@
 package com.test.games;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 /**
  * 
@@ -30,36 +32,36 @@ public class NumberGuessingGameV7 {
 	  
 	  static int playGame(int lower_limit, int upper_limit)
 	  {
-			
-			String temp=null; 
-			
-			
-			 
-			
-			
-			
+		 boolean flag=false;
+		  String[] strArr= {"yes","end","higher","lower"};
+		  List<String> list=new ArrayList<String>();
+		  list=Arrays.asList(strArr);
+		  String temp=""; 
 			//decl scanner
 			Scanner kbinput = new Scanner(System.in);
+			//take a first guess - avg of upper limit
 			int numberToGuess=upper_limit/2;
-			//take a first guess
-			info("Is the number: "+numberToGuess);
+			
+			info("Is it: "+numberToGuess+ " ?");
 			info("If Yes, type 'end' or 'yes';if not Type either 'higher' or 'lower're to proceed with guess");
 			
 			 temp=kbinput.nextLine(); 
 		
 			
 			 //if it is not yes or end
-			 if(!temp.equalsIgnoreCase("yes") 
-					|| !temp.equalsIgnoreCase("end") )
+			 if (!temp.equalsIgnoreCase("yes") ||
+					!temp.equalsIgnoreCase("end"))	
 			 {
 				
 				//if it is equal to higher or lower				 
-				while(temp.equalsIgnoreCase("higher") || temp.equalsIgnoreCase("lower"))
+				while(temp.equalsIgnoreCase("higher") ||
+						temp.equalsIgnoreCase("lower"))
 				{
 					
 					if(temp.equalsIgnoreCase("higher"))
 					{
 						//info("inside higher");
+						if(numberToGuess >=100) break;
 						lower_limit=numberToGuess;
 						lower_limit++;
 						
@@ -67,6 +69,7 @@ public class NumberGuessingGameV7 {
 					else if(temp.equalsIgnoreCase("lower"))
 					{
 						//info("inside lower");
+						if(numberToGuess <=1) break;
 						upper_limit=numberToGuess;
 						upper_limit--;
 					}
@@ -75,29 +78,23 @@ public class NumberGuessingGameV7 {
 					//info("lower_limit:"+lower_limit);
 					
 					numberToGuess=(upper_limit+lower_limit)/2;
-					info("Is the number: "+numberToGuess );
+					info("Is it : "+numberToGuess +" ?" );
 					temp=kbinput.nextLine();
+					
 				}
 				
 			}
 			 
-			if(!temp.equalsIgnoreCase("higher") || !temp.equalsIgnoreCase("lower"))
-			{    numberToGuess=-1;
-				info("***Thanks ***");
-				
-			}
-			else
-				if(temp.equalsIgnoreCase("yes") || temp.equalsIgnoreCase("end"))
-			{
-				info("Number guessed is: "+numberToGuess);
-				info("Thanks for playing the game");
-			}
-			 
-			
-			 kbinput.close();
-			 return numberToGuess;
+			 for(String str: list) {
+				    if(str.trim().contains(temp))
+				    	info("Number guessed is: "+numberToGuess);
+				        flag=true;
+				    	
+				}
+				 
+			 	//logic for invalid input
+				 if(!flag) numberToGuess=-1;		 
 		
-	  }
-	  
-	  
-}
+				 return numberToGuess;
+			 
+	  }}
